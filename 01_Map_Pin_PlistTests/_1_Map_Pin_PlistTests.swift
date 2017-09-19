cd14474f366e8485efb7ff94fbd1d47a23230986
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2017-09-19 11:18:17 +0900

    Initial Commit

---
+++ 01_Map_Pin_PlistTests/_1_Map_Pin_PlistTests.swift
@@ -0,0 +1,36 @@
+//
+//  _1_Map_Pin_PlistTests.swift
+//  01_Map_Pin_PlistTests
+//
+//  Created by D7703_08 on 2017. 9. 19..
+//  Copyright © 2017년 D7703_08. All rights reserved.
+//
+
+import XCTest
+@testable import _1_Map_Pin_Plist
+
+class _1_Map_Pin_PlistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
